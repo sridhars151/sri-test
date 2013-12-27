590cfc1a652e7d0b0a7d51f53bbbe1597de24e6b
unknown <sridhars151@gmail.com>
2013-12-27 16:54:02 +0530

    initial setup

---
+++ archive.php
@@ -0,0 +1,299 @@
+<?php
+/**
+ * The Template for displaying all single posts.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.0
+ */
+
+//get_header(); ?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<!-- Mirrored from www.aglaze.com/news/ by HTTrack Website Copier/3.x [XR&CO'2013], Tue, 22 Oct 2013 04:49:38 GMT -->
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+<head>
+<title>AGlaze USA</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta name="description" content="Default Description" />
+<meta name="keywords" content="Magento, Varien, E-commerce" />
+<meta name="robots" content="INDEX,FOLLOW" />
+<link rel="icon" href="../media/favicon/default/logo.png" type="image/x-icon" />
+<link rel="shortcut icon" href="../media/favicon/default/logo.png" type="image/x-icon" />
+<!--[if lt IE 7]>
+<script type="text/javascript">
+//<![CDATA[
+    var BLANK_URL = 'http://www.aglaze.com/js/blank.html';
+    var BLANK_IMG = 'http://www.aglaze.com/js/spacer.gif';
+//]]>
+</script>
+<![endif]-->
+<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory');?>/skin/frontend/default/aglaze/css/styles.css" media="all" />
+<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory');?>/skin/frontend/default/aglaze/css/widgets.css" media="all" />
+<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory');?>/skin/frontend/default/aglaze/css/magento-team/cloud-zoom.css" media="all" />
+<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory');?>/skin/frontend/default/aglaze/css/print.css" media="print" />
+<script type="text/javascript" src="<?php bloginfo('template_directory');?>/js/prototype/prototype.js"></script>
+<script type="text/javascript" src="<?php bloginfo('template_directory');?>/js/lib/ccard.js"></script>
+<script type="text/javascript" src="<?php bloginfo('template_directory');?>/js/prototype/validation.js"></script>
+<script type="text/javascript" src="<?php bloginfo('template_directory');?>/js/scriptaculous/builder.js"></script>
+<script type="text/javascript" src="<?php bloginfo('template_directory');?>/js/scriptaculous/effects.js"></script>
+<script type="text/javascript" src="<?php bloginfo('template_directory');?>/js/scriptaculous/dragdrop.js"></script>
+<script type="text/javascript" src="<?php bloginfo('template_directory');?>/js/scriptaculous/controls.js"></script>
+<script type="text/javascript" src="<?php bloginfo('template_directory');?>/js/scriptaculous/slider.js"></script>
+<script type="text/javascript" src="<?php bloginfo('template_directory');?>/js/varien/js.js"></script>
+<script type="text/javascript" src="<?php bloginfo('template_directory');?>/js/varien/form.js"></script>
+<script type="text/javascript" src="<?php bloginfo('template_directory');?>/js/varien/menu.js"></script>
+<script type="text/javascript" src="<?php bloginfo('template_directory');?>/js/mage/translate.js"></script>
+<script type="text/javascript" src="<?php bloginfo('template_directory');?>/js/mage/cookies.js"></script>
+<script type="text/javascript" src="<?php bloginfo('template_directory');?>/js/jquery/jquery-1.4.2.min.js"></script>
+<script type="text/javascript" src="<?php bloginfo('template_directory');?>/js/fishpig/wordpress/main.js"></script>
+<script type="text/javascript" src="<?php bloginfo('template_directory');?>/skin/frontend/default/aglaze/js/magento-team/cloud-zoom.1.0.2.min.js"></script>
+<link rel="canonical" href="index.html" />
+<!--[if lt IE 8]>
+<link rel="stylesheet" type="text/css" href="http://www.aglaze.com/skin/frontend/default/aglaze/css/styles-ie.css" media="all" />
+<![endif]-->
+<!--[if lt IE 7]>
+<script type="text/javascript" src="http://www.aglaze.com/js/lib/ds-sleight.js"></script>
+<script type="text/javascript" src="http://www.aglaze.com/skin/frontend/base/default/js/ie6.js"></script>
+<![endif]-->
+
+<script type="text/javascript">
+//<![CDATA[
+optionalZipCountries = [];
+//]]>
+</script>
+<link rel="alternate" type="application/rss+xml" title="AGlaze Latest News - RSS Feed" href="feed" />
+<script type="text/javascript">var Translator = new Translate({"Please enter a valid phone number. For example (123) 456-7890 or 123-456-7890.":"Please enter a valid phone number. For example 01234 567890,  (123) 456-7890 or 123-456-7890.","Please enter a valid email address. For example johndoe@domain.com.":"Please enter a valid email address. For example johndoe@example.com.","Please enter a valid zip code. For example 90602 or 90602-1234.":"Please enter a valid postcode or zip. For example AB1 1AB, 90602 or 90602-1234.","Please enter a valid zip code.":"Please enter a valid postcode or zip.","Please enter a valid $ amount. For example $100.00.":"Please enter a valid \u00a3 amount. For example \u00a3100.00.","Please select State\/Province.":"Please select County\/State.","Please use only letters (a-z or A-Z), numbers (0-9) or underscore(_) in this field, first character should be a letter.":"Please use only letters (a-z or A-Z), numbers (0-9) or underscores (_) in this field, first character must be a letter.","Your session has been expired, you will be relogged in now.":"Your session has expired, you will be re-logged in now."});</script><script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-33902676-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+</head>
+<body class=" wordpress-homepage-index is-blog && page-bg">
+<div class="wrapper">
+        <noscript>
+        <div class="noscript">
+            <div class="noscript-inner">
+                <p><strong>JavaScript seem to be disabled in your browser.</strong></p>
+                <p>You must have JavaScript enabled in your browser to utilize the functionality of this website.</p>
+            </div>
+        </div>
+    </noscript>
+    <div class="page">
+        <div class="header-container">
+        
+    <div class="header">
+                <div class="logo"><strong>Aglaze</strong><a href="http://aglazeusa.com" title="Aglaze"><img style="float:left;" src="http://aglazeusa.com/wp-content/uploads/2013/12/logo.png" alt="Aglaze" />
+<img style="float:left; margin-left:-30px;" src="http://aglazeusa.com/wp-content/uploads/2013/11/usa1.png" width="85" height="60" alt="flag"
+</a></div>
+               
+       
+           <div class="quick-access">
+           
+                <!--- <img class="icon" src="<?php bloginfo('template_directory');?>/images/RSS_button_1021.png" alt="checkout your basket" style="margin:5px; position:relative; left:15px;" />--->
+
+<a href="http://aglazeusa.com/feed/" title="Subscribe to my feed" rel="alternate" type="application/rss+xml"><img class="icon" src="//feedburner.google.com/fb/images/pub/feed-icon32x32.png" alt="" style="position:relative; left:15px;"/></a>
+       
+        <img class="icon inline" src="<?php bloginfo('template_directory');?>/skin/frontend/default/aglaze/images/_aglaze/phone.gif" alt="call us today" />
+        <p class="tel inline">Telephone: <span class="aglaze-number">+1-303-586-4992</span></p>
+       
+    </div>
+   
+    
+       
+    </div>
+    <div id="nav-aglaze">
+          <div>
+
+	<!--Menu-->
+        <?php
+
+		$defaults = array(
+			'theme_location'  => 'primary',
+			'container'       => 'div',
+			'container_class' => 'nav-container',
+			'container_id'    => '',
+			'menu_class'      => '',
+			'menu_id'         => 'nav',
+			'echo'            => true,
+			'fallback_cb'     => 'wp_page_menu',
+			'before'          => '',
+			'after'           => '',
+			'link_before'     => '',
+			'link_after'      => '',
+			'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+			'depth'           => 0,
+			'walker'          => ''
+		);
+		
+		wp_nav_menu( $defaults );
+		
+		?>
+        
+        <!--Menu-->
+
+<!--<form id="search_mini_form" action="" method="get">
+    <div class="form-search">
+               <input id="search" type="text" name="q" value="" class="input-text" />
+        <button type="submit" title="Go" class="button"><span><span>Go</span></span></button>
+        <div id="search_autocomplete" class="search-autocomplete"></div>
+        <script type="text/javascript">
+        //<![CDATA[
+            var searchForm = new Varien.searchForm('search_mini_form', 'search', 'Search entire shop here...');
+            searchForm.initAutocomplete('../catalogsearch/ajax/suggest/index.html', 'search_autocomplete');
+        //]]>
+        </script>
+    </div>
+</form>--></div>     </div>
+     
+      
+
+        </div>	
+        
+        <div class="main-container col2-left-layout">
+            <div class="main">
+                                <div class="col-main">
+              
+              
+
+			<?php
+			/* Start the Loop */
+			while ( have_posts() ) : the_post();
+
+				/* Include the post format-specific template for the content. If you want to
+				 * this in a child theme then include a file called called content-___.php
+				 * (where ___ is the post format) and that will be used instead.
+				 */
+				get_template_part( 'content', get_post_format() );
+
+			endwhile;
+
+			twentytwelve_content_nav( 'nav-below' );
+			?>
+
+  
+                                
+                                
+                                </div>
+                <div class="col-left sidebar"><div class="wp-sidebar">
+	<div class="block block-blog block-blog-search">
+			<div class="block-title">
+			<strong><span>Blog Search</span></strong>
+		</div>
+		<div class="block-content">
+		<form role="search" method="get" id="searchform" class="searchform" action="http://aglazeusa.com/">
+				<div>
+					<label class="screen-reader-text" for="s">Search for:</label>
+					<input value="" name="s" id="s" type="text">
+					<input id="searchsubmit" value="Search" type="submit">
+				</div>
+</form>
+	</div>
+</div>
+
+
+
+<div class="block block-rss">
+	        <div class="block-title">
+			<strong><span>Subscribe Our feed</span></strong>
+		</div>
+		<div class="block-content">
+		<form action="http://feedburner.google.com/fb/a/mailverify" method="post" target="popupwindow" onsubmit="window.open('http://feedburner.google.com/fb/a/mailverify?uri=aglazeusa/GMOY', 'popupwindow', 'scrollbars=yes,width=550,height=520');return true">
+    <p>Enter your email address:</p>
+    <p><input type="text" style="width:140px" name="email"/></p>
+    <input type="hidden" value="aglazeusa/GMOY" name="uri"/>
+    <input type="hidden" name="loc" value="en_US"/>
+    <input type="submit" value="Subscribe" />
+                </form>
+	</div>
+</div>
+
+
+
+
+
+	<div class="block block-blog block-blog-categories">
+					<div class="block-title">
+				<strong><span>Categories</span></strong>
+			</div>
+				<div class="block-content">
+			<ul class="level0" id="wp-category-list">
+															<li class="level0 item">
+							<a  class="level0" href="" title="Aviaton News">
+								Aviaton News							</a>																				</li>
+											<li class="level0 item">
+							<a  class="level0" href="" title="Latest News">
+								Latest News							</a>																				</li>
+											<li class="level0 item">
+							<a  class="level0" href="" title="Marine News">
+								Marine News							</a>																				</li>
+											<li class="level0 item">
+							<a  class="level0" href="" title="Products News">
+								Products News							</a>																				</li>
+											<li class="level0 item">
+							<a  class="level0" href="" title="Uncategorized">
+								Uncategorized							</a>																				</li>
+												</ul>
+			<script type="text/javascript">decorateList('wp-category-list')</script>
+		</div>
+	</div>
+	<div class="block block-blog block-blog-archives">
+					<div class="block-title">
+				<strong><span>Archives</span></strong>
+			</div>
+				<div class="block-content">
+			<ul id="wp-archive-list">
+					<ul id="wp-archive-list">
+                         <?php wp_get_archives( $args );
+$args = array(
+	'type'            => 'monthly',
+	'limit'           => '10',
+	'format'          => 'link', 
+	'before'          => '',
+	'after'           => '',
+	'show_post_count' => true,
+	'echo'            => 1,
+	'order'           => 'DESC'
+);
+
+
+
+?>    
+                            
+                        </ul> 
+										
+			
+		</div>
+	</div>
+</div>
+
+</div>
+            
+          </div>  
+         
+                  <div id="aglaze-services-wrap">   
+        <div class="aglaze-services"> <img class="fl" src="http://aglazeusa.com/wp-content/themes/twentytwelve/skin/frontend/default/aglaze/images/_aglaze/automotive.jpg" />
+        <p class="fr">Automotive</p>
+        <a href=""><img class="fr" src="http://aglazeusa.com/wp-content/themes/twentytwelve/skin/frontend/default/aglaze/images/_aglaze/more-info.jpg" /></a> </div>
+        <div class="aglaze-services aglaze-middle"> <img class="fl" src="http://aglazeusa.com/wp-content/themes/twentytwelve/skin/frontend/default/aglaze/images/_aglaze/marine.jpg" />
+        <p class="fr">Marine</p>
+        <a href=""><img class="fr" src="http://aglazeusa.com/wp-content/themes/twentytwelve/skin/frontend/default/aglaze/images/_aglaze/more-info.jpg" /></a> </div>
+        <div class="aglaze-services"> <img class="fl" src="http://aglazeusa.com/wp-content/themes/twentytwelve/skin/frontend/default/aglaze/images/_aglaze/aviation.jpg" />
+        <p class="fr">Aviation</p>
+                <a href=""><img class="fr" src="http://aglazeusa.com/wp-content/themes/twentytwelve/skin/frontend/default/aglaze/images/_aglaze/more-info.jpg" /></a> </div>
+        </div>
+        
+        
+        </div>
+        </div>	
+<?php get_footer(); ?>
